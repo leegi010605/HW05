8b119ef58727421fbe462e89b2a18f92065f203a
leegi010605 <91926365+leegi010605@users.noreply.github.com>
2022-04-13 01:23:38 +0900

    split(rm special words)

--- word_freq.py
+++ word_freq.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import re
 
 txtf=str(sys.argv[1])
 num=int(sys.argv[2])
@@ -10,7 +11,16 @@ print(sys.argv[1],sys.argv[2])
 f=open(txtf,'r')
 
 lines=f.readlines()
-for line in lines:
-    print(line)
+
+for i in lines:
+    i = re.sub("[-=+,#/\?:^.@*\"※~ㆍ!]", "", i) 
+    words=i.split()
+    #for word in words:
+
+print(words)
+
+#for i in range(0,len(sep),1):
+   # if t_rmv in sep:
+        
 
 f.close()
